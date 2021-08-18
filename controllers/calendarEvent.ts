452ee3b1560cd501aaf15f0ca92d57bf290d952b
Ivan Contreras <ivanc.contre@gmail.com>
2021-08-18 16:06:16 -0400

    ADD: Backend 1.0 terminado

--- controllers/calendarEvent.ts
+++ controllers/calendarEvent.ts
@@ -16,14 +16,27 @@ interface CustomResponse extends Response {
 
 export const getCalendarEvents = async (req: Request, res: Response): Promise<CustomResponse> => {
 
-    return res.json({
-        ok: true,
-        msg: 'getCalendarEvents'    
-    });
+    try {
+
+        const calendarEvents = await CalendarEventModel.find({}).populate('user', 'name');
+
+        return res.json({
+            ok: true,
+            body: {
+                calendarEvents
+            }  
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }    
 };
 
 export const createCalendarEvent = async (req: Request, res: Response): Promise<CustomResponse> => {
-
 
     const calendarEvent = new CalendarEventModel(req.body);
 
@@ -51,16 +64,87 @@ export const createCalendarEvent = async (req: Request, res: Response): Promise<
 
 export const updateCalendarEvent = async (req: Request, res: Response): Promise<CustomResponse> => {
 
-    return res.json({
-        ok: true,
-        msg: 'updateCalendarEvent'    
-    });
+    const calendarEventId = req.params.id;
+    const uid = req.body.uid;
+
+    try {
+        
+        const calendarEvent = await CalendarEventModel.findById(calendarEventId);
+
+        if (!calendarEvent) {
+            return res.status(400).json({
+                ok: false,
+                msg: `CalendarEvent no existe con el id ${calendarEventId}`
+            });
+        }
+
+        if (calendarEvent.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene prvilegio para editar el CalendarEvent'
+            });
+        }
+
+        const bodyNewCalendarEvent = {
+            ...req.body,
+            user: uid
+        };
+
+        const calendarEventUpdated = await CalendarEventModel.findByIdAndUpdate(calendarEventId, bodyNewCalendarEvent, { new: true });
+
+        return res.json({
+            ok: true,
+            body: {
+                calendarEventUpdated
+            }   
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
+    
 };
 
 export const deleteCalendarEvent = async (req: Request, res: Response): Promise<CustomResponse> => {
 
-    return res.json({
-        ok: true,
-        msg: 'deleteCalendarEvent'    
-    });
+    const calendarEventId = req.params.id;
+    const uid = req.body.uid;
+
+    try {
+        
+        const calendarEvent = await CalendarEventModel.findById(calendarEventId);
+
+        if (!calendarEvent) {
+            return res.status(400).json({
+                ok: false,
+                msg: `CalendarEvent no existe con el id ${calendarEventId}`
+            });
+        }
+
+        if (calendarEvent.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene prvilegio para eliminar el CalendarEvent'
+            });
+        }
+
+        await CalendarEventModel.findByIdAndDelete(calendarEventId);
+
+        return res.json({
+            ok: true,
+            body: {}   
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
+
 };
